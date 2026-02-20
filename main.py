e8ff2c31b5330006ad0ab8291b53a6a8f184c56a
Leniwcowaty <krystian.kacprzak.98@gmail.com>
2026-02-20 16:46:57 +0100

    Code refactor

--- main.py
+++ main.py
@@ -1,62 +1,16 @@
-try:
-    import sys
-except ModuleNotFoundError:
-    print("Module sys is not installed")
-
-try:
-    import os
-except ModuleNotFoundError:
-    print("Module os is not installed")
+import sys
+import utils
 
 try:
     import requests as r
-except ModuleNotFoundError:
-    print("Module requests is not installed")
-
-try:
     import json
-except ModuleNotFoundError:
-    print("Module json is not installed")
-
-try:
-    import yaml
-except ModuleNotFoundError:
-    print("Module yaml is not installed")
+except ImportError as e:
+    sys.exit(f"Module {e.name} not found. Please install correct module.")
 
 endpointUrl = "https://api.gios.gov.pl/pjp-api/v1/rest/data/getData/"
 
-def read_config():
-    configPath = os.path.expanduser('~/.config/giosifier/config.yaml')
-    try:
-        with open(configPath, 'r') as configFile:
-            config = yaml.safe_load(configFile)
-    except FileNotFoundError:
-        sys.exit(f"Config file not found in {configPath}")
-
-    pm10Config = {"name": "Pył PM10", "id": config["pm10"]["sensor_id"], "alert": config["pm10"]["alert"]}
-    no2Config = {"name": "Dwutlenek azotu (NO2)", "id": config["no2"]["sensor_id"], "alert": config["no2"]["alert"]}
-    try:
-        nftyConfig = {"url": config["ntfy"]["url"], "auth": config["ntfy"]["auth"]}
-    except KeyError:
-        nftyConfig = {"url": config["ntfy"]["url"], "auth": None}
-
-    configList = [pm10Config, no2Config, nftyConfig]
-
-    return configList
-    
-def parse_response(json, obj):
-    level = json["Lista danych pomiarowych"][0]["Wartość"]
-    station = json["Lista danych pomiarowych"][0]["Kod stanowiska"]
-    measurement = json["Lista danych pomiarowych"][0]["Data"]
-    if level > obj["alert"]:
-        body = "Stacja \"" + station + "\": Poziom " + obj["name"] + " za wysoki! Wartość: " + str(level) + " (data pomiaru: " + measurement + ")\n"
-        result = {"message": body, "triggered": True}
-    else:
-        result = {"message": None, "triggered": False}
-    return result
-
 def main():
-    config = read_config()
+    config = utils.read_config()
 
     apiHeader = {"Authorization": config[2]["auth"]}
     try:
@@ -65,8 +19,8 @@ def main():
     except ConnectionError:
         sys.exit("Connection error when trying to connect to API")
 
-    pm10Status = parse_response(pm10Response, config[0])
-    no2Status = parse_response(no2Response, config[1])
+    pm10Status = utils.parse_response(pm10Response, config[0])
+    no2Status = utils.parse_response(no2Response, config[1])
 
     if pm10Status["triggered"]:
         r.post(config[2]["url"], headers=apiHeader, data=pm10Status["message"])
---
+++ utils.py
@@ -0,0 +1,52 @@
+import sys
+try:
+    import os
+    import requests as r
+    import json
+    import yaml
+except ImportError as e:
+    sys.exit(f"Module {e.name} not found. Please install correct module.")
+
+endpointUrl = "https://api.gios.gov.pl/pjp-api/v1/rest/station/sensors/"
+
+def read_config():
+    configPathGlobal = os.path.expanduser('~/.config/giosifier/config.yaml')
+    configPathLocal = ('/etc/giosifier/config.yaml')
+    try:
+        with open(configPathLocal, 'r') as configFile:
+            config = yaml.safe_load(configFile)
+    except FileNotFoundError:
+        try:
+            with open(configPathGlobal, 'r') as configFile:
+                config = yaml.safe_load(configFile)
+        except FileNotFoundError as e:
+            sys.exit(f"Config file not found: {e}")
+
+    stationInfo = json.loads(r.get(endpointUrl+str(config["station"]["id"])).text)["Lista stanowisk pomiarowych dla podanej stacji"]
+    sensors = {item["Wskaźnik - kod"]: item["Identyfikator stanowiska"] for item in stationInfo}
+
+    try:
+        pm10Config = {"name": "Pył PM10", "id": str(sensors["PM10"]), "alert": config["pm10"]["alert"]}
+        no2Config = {"name": "Dwutlenek azotu (NO2)", "id": str(sensors["NO2"]), "alert": config["no2"]["alert"]}
+    except KeyError as e:
+        sys.exit(f"This station does not have {e} sensor")
+    
+    try:
+        nftyConfig = {"url": config["ntfy"]["url"], "auth": config["ntfy"]["auth"]}
+    except KeyError:
+        nftyConfig = {"url": config["ntfy"]["url"], "auth": None}
+
+    configList = [pm10Config, no2Config, nftyConfig]
+
+    return configList
+    
+def parse_response(json, obj):
+    level = json["Lista danych pomiarowych"][0]["Wartość"]
+    station = json["Lista danych pomiarowych"][0]["Kod stanowiska"]
+    measurement = json["Lista danych pomiarowych"][0]["Data"]
+    if level > obj["alert"]:
+        body = "Stacja \"" + station + "\": Poziom " + obj["name"] + " za wysoki! Wartość: " + str(level) + " (data pomiaru: " + measurement + ")\n"
+        result = {"message": body, "triggered": True}
+    else:
+        result = {"message": None, "triggered": False}
+    return result
